767a2d137327047f6997ef6934250b134efab2f7
Roderick Cassar <roderickcassar@Rodericks-MacBook-Pro.local>
2017-02-22 09:11:41 +0100

    Version 0.1

--- js/main.js
+++ js/main.js
@@ -1,118 +1,48 @@
-(function($){
-    $.fn.scrollingTo = function( opts ) {
-        var defaults = {
-            animationTime : 1000,
-            easing : '',
-            callbackBeforeTransition : function(){},
-            callbackAfterTransition : function(){}
-        };
-
-        var config = $.extend( {}, defaults, opts );
-
-        $(this).click(function(e){
-            var eventVal = e;
-            e.preventDefault();
-
-            var $section = $(document).find( $(this).data('section') );
-            if ( $section.length < 1 ) {
-                return false;
-            };
-
-            if ( $('html, body').is(':animated') ) {
-                $('html, body').stop( true, true );
-            };
-
-            var scrollPos = $section.offset().top;
-
-            if ( $(window).scrollTop() == scrollPos ) {
-                return false;
-            };
-
-            config.callbackBeforeTransition(eventVal, $section);
-
-            $('html, body').animate({
-                'scrollTop' : (scrollPos+'px' )
-            }, config.animationTime, config.easing, function(){
-                config.callbackAfterTransition(eventVal, $section);
-            });
-        });
-    };
-}(jQuery));
-
-
-
-jQuery(document).ready(function(){
-	"use strict";
-	new WOW().init();
-
-
-(function(){
- jQuery('.smooth-scroll').scrollingTo();
-}());
-
-});
-
-
-
-
-$(document).ready(function(){
-
-
-
-
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 50) {
-            $(".navbar-brand a").css("color","#fff");
-            $("#top-bar").removeClass("animated-header");
-        } else {
-            $(".navbar-brand a").css("color","inherit");
-            $("#top-bar").addClass("animated-header");
-        }
-    });
-
-    $("#clients-logo").owlCarousel({
- 
-        itemsCustom : false,
-        pagination : false,
-        items : 5,
-        autoplay: true,
-
-    })
-
-});
-
-
-
-// fancybox
-$(".fancybox").fancybox({
-    padding: 0,
-
-    openEffect : 'elastic',
-    openSpeed  : 450,
-
-    closeEffect : 'elastic',
-    closeSpeed  : 350,
-
-    closeClick : true,
-    helpers : {
-        title : { 
-            type: 'inside' 
-        },
-        overlay : {
-            css : {
-                'background' : 'rgba(0,0,0,0.8)'
-            }
-        }
-    }
-});
-
-
-
-
-
-
- 
-
-
-
-
+	jQuery(document).ready(function($){
+		"use strict";
+		
+		var demo1 = $("#tada-slider").slippry({
+			speed: 1000,
+			pager:false,
+			captions:false
+		});		
+		
+		/* PRELOADER */
+		$("#preloader-container").delay(2000).fadeOut("slow");
+		
+		/* STICKY MENU */
+		var stickyTop = $('nav.menu-sticky').offset().top;
+		$(window).scroll(function(){
+			if ($(this).scrollTop() >= stickyTop) {
+				$('nav').addClass('sticky');
+			} else {
+				$('nav').removeClass('sticky');
+			}
+		})
+	
+		/* BACK TO TOP */
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > 50) {
+				$('.backtotop').fadeIn('slow');
+			} else {
+				$('.backtotop').fadeOut('slow');
+			}
+		});
+		$('.backtotop').on("click", function(){
+			$("html, body").animate({ scrollTop: 0 }, 500);        
+			return false;
+		});	
+		
+		$('.open-menu-responsive').on("click", function(){
+			$('.menu-responsive').css('display','block');
+			$('.close-menu-responsive').css('display','block');
+			$('.open-menu-responsive').css('display','none');
+			$('header').addClass('header-menu-open');
+		});
+		$('.close-menu-responsive').on("click", function(){
+			$('.menu-responsive').css('display','none');
+			$('.close-menu-responsive').css('display','none');
+			$('.open-menu-responsive').css('display','block');
+			$('header').removeClass('header-menu-open');
+		});				
+	});
